bf5a3ac126172f8a7712d7d6ff29cd2034266ff9
Reazul Islam Reaz <reazulislam1487@gmail.com>
2025-10-24 23:57:22 +0600

    added TestForm

--- src/components/TestForm.tsx
+++ src/components/TestForm.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 const TestForm = () => {
     return (
         <div>
-            
+            <form >
+                <input type="text" name='name'  />
+            </form>
         </div>
     );
 };
